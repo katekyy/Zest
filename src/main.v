ef856820d4e1149dae54f781300cc6877b1c7cf8
katekyy <deffkatek@gmail.com>
2023-11-14 22:31:18 +0100

    Test the new JsonRPC implementation.

--- src/main.v
+++ src/main.v
@@ -2,40 +2,44 @@ module main
 
 import auth
 import user
-import rpc.tcp
-import rpc
+import jsonrpc
+// import rpc.tcp
+// import rpc
 
 fn main() {
-	mut srv := tcp.new_server(none, none)!
+	mut rpc := jsonrpc.new_tcp_server(none, none, 500)!
 	mut authsrv := auth.AuthServer{
 		client_id: '43e7a021-38ba-4df5-a9ef-7cbe5594aec0'
 		port: 8000
 	}
-	dump(srv.address)
-	srv.register(
+	rpc.register(
 		method: 'get_login_url'
-		func: fn [mut authsrv] (mut r rpc.Server, p rpc.Params) !rpc.Response {
-			return r.ok(authsrv.init())
+		func: fn [mut authsrv] (p jsonrpc.Params) !jsonrpc.MethodResponse {
+			return jsonrpc.respond(result: authsrv.init())
 		}
 	)
-	srv.ok() or { println('Error: ' + err.msg()) }
+	dump(rpc.address())
+	rpc.wait()!
+	// 	 rpc.request_json('[{"jsonrpc":"2.0","method":"sum","params":{"0":"2", "1":"2"},"id":"0"},
+	// {"jsonrpc":"2.0","method":"sum","params":{"0":"10", "1":"20"},"id":"1"}]')
+	// dump(rpc.wait_json())
 
+	// mut srv := tcp.new_server(none, none)!
+	// dump(srv.address)
 	// srv.register(
-	// 	method: 'get_user'
-	// 	func: fn [usr] (_ string) !string {
-	// 		return '${usr.json()}'
+	// 	method: 'get_login_url'
+	// 	func: fn [mut authsrv] (mut r rpc.Server, p rpc.Params) !rpc.Response {
+	// 		return r.ok(authsrv.init())
 	// 	}
 	// )
-	// srv.request(rpc.Request{
-	// 	method: 'get_user'
-	// 	params: ''
-	// 	id: '0'
-	// })
-	// // srv.json('{"method":"test"}')
-	// for {
-	// 	dump(json.encode(srv.response() or { continue }))
-	// 	time.sleep(1 * time.second)
-	// }
+	// srv.register(
+	// 	method: 'refresh'
+	// 	func: fn (mut r rpc.Server, p rpc.Params) !rpc.Response {
+	// 		a := p[''] or { '' }
+	// 		return r.ok(none)
+	// 	}
+	// )
+	// srv.ok() or { println('Error: ' + err.msg()) }
 
 	// TODO RUN THE FLUTTER/AVALONIA APP AND WAIT
 }
